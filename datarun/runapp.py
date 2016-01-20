a2602e62dbc3843872286a36c06745dc82a4309d
camille24 <camille.marini@gmail.com>
2016-01-20 11:13:08 +0100

    first views

--- datarun/runapp.py
+++ datarun/runapp.py
@@ -2,20 +2,30 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-# from flask import jsonify, abort, make_response
-# from flask.ext.httpauth import HTTPBasicAuth
+from flask import jsonify  # , abort, make_response
+from flask.ext.httpauth import HTTPBasicAuth
 # from flask.ext.restful import Api, Resource, reqparse, fields, marshal
+from .models import SubmissionFold
 
 app = Flask(__name__)
 app.config.from_object(os.getenv('APP_SETTINGS'))
 db = SQLAlchemy(app)
 
-# auth = HTTPBasicAuth()
+auth = HTTPBasicAuth()
 
-# @app.route('/todo/api/v1.0/tasks', methods=['GET'])
-# @auth.login_required
-# def get_tasks():
-#    return jsonify({'tasks': tasks})
+
+@app.route('/submissions_fold/', methods=['GET'])
+@auth.login_required
+def index():
+    return jsonify({'submissions_fold': SubmissionFold.query.all()})
+
+
+@app.route('/submissions_fold/<int:id>')
+def get_submission_state(id):
+    return jsonify({'submission_fold': SubmissionFold.query.get(id)})
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
