e3877a1e05c0704eadb2aed7f3c25e63b102ec4a
camille24 <camille.marini@gmail.com>
2016-02-05 17:41:54 +0100

    changing id of submission and submission fold

--- datarun/runapp/models.py
+++ datarun/runapp/models.py
@@ -22,36 +22,65 @@ class RawData(models.Model):
     target_column = models.CharField(max_length=200, null=False)
 
     def __unicode__(self):
-        return 'RawData( id {}, name {}, file_path {} )'.format(self.id,
-                                                                self.name,
-                                                                self.files_path)
+        return 'RawData(id {}, name {}, file_path {})'.format(self.id,
+                                                              self.name,
+                                                              self.files_path)
 
 
 class Submission(models.Model):
     """
-    :param databoard_id: id of the submission in the db of databoard
+    :param databoard_s_id: id of the submission in the db of databoard
     :param files_path: path of submitted files
     :param raw_data: associated raw data
 
-    :type databoard_id: IntegerField(primary_key=True)
+    :type databoard_s_id: IntegerField(primary_key=True)
     :type files_path: FileField(max_length=200, null=True)
     :type raw_data: ForeignKey(RawData, null=True, blank=True)
     """
-    databoard_id = models.IntegerField(primary_key=True)
+    databoard_s_id = models.IntegerField(primary_key=True)
     files_path = models.CharField(max_length=200, null=True)
     raw_data = models.ForeignKey(RawData, null=True, blank=True)
 
     def __unicode__(self):
-        return 'Submission( id {}, files_path {}, raw_data_id {} )'. \
-                format(self.id, self.files_path, self.raw_data_id)
+        return 'Submission(databoard id {}, files_path {}, raw_data_id {})'. \
+                format(self.databoard_s_id, self.files_path, self.raw_data_id)
 
 # TODO? put files in the database
 # class SubmissionFile(models.Model):
 
 
 class SubmissionFold(models.Model):
+    """
+    :param databoard_sf_id: id of the submission on cv fold in databoard db
+    :param submission: associated submission
+    :param train_is: train indices
+    :param test_is!: test indices
+    :param full_train_predictions: predictions of the entire train dataset
+    :param test_predictions: predictions of the test dataset
+    :param state: TODO, TRAINED, VALIDATED, TESTED, ERROR
+    :param log_messages: logs recorded during train and test
+    :param train_time: real clock training time
+    :param validation_time: real clock validation time
+    :param test_time: real clock testing time
+    :param train_cpu_time:  training cpu time
+    :param train_memory: training memory usage
 
-    databoard_id = models.IntegerField(primary_key=True)
+    :type databoard_sf_id: IntegerField(primary_key=True)
+    :type submission: ForeignKey(Submission, null=True, blank=True)
+    :type train_is: TextField
+    :type test_is: TextField
+    :type full_train_predictions: TextField
+    :type test_predictions: TextField
+    :type state: CharField, choices.
+    :type log_messages: TextField
+    :type train_time: FloatField, default=0.
+    :type validation_time: FloatField, default=0.
+    :type test_time: FloatField, default=0.
+    :type train_cpu_time: FloatField, default=0.
+    :type train_memory: FloatField, default=0.
+    """
+
+    databoard_sf_id = models.IntegerField(primary_key=True)
     submission = models.ForeignKey(Submission, null=True, blank=True)
     train_is = models.TextField(null=False)
     test_is = models.TextField(null=False)
@@ -75,5 +104,5 @@ class SubmissionFold(models.Model):
     train_memory = models.FloatField(default=0., null=True)
 
     def __unicode__(self):
-        return 'SubmissionFold( id {}, submission_id {}, state {} )'. \
-                format(self.id, self.submission_id, self.state)
+        return 'SubmissionFold(databoard id {}, submission_id {}, state {})'. \
+                format(self.databoard_sf_id, self.submission_id, self.state)
