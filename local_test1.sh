0415e8e1f675d5abc59d8ccb59b3218369ff3600
camille24 <camille.marini@gmail.com>
2016-03-02 15:48:09 +0100

    cleaning local celery tests

---
+++ local_test1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# :Usage: bash local_test1.sh  
+# Set up test environment: creation of a test database, starting the workers,
+# and running a development server
+# local_test2.sh must be run afterward
+
+# Set up some environment variables
+export DIR_DATA="test_data"
+export DIR_SUBMISSION="test_submission"
+export CELERY_SCHEDULER_PERIOD='*/1'
+rm -r $DIR_DATA/*
+rm -r $DIR_SUBMISSION/*
+touch $DIR_SUBMISSION/__init__.py
+
+# Create a test database
+export DR_DATABASE_NAME="test_datarun"
+createdb $DR_DATABASE_NAME
+# Apply migrations
+python manage.py migrate
+
+# Create a test user
+python manage.py createuser MrTest test@test.com test 
+
+# Start celery workers
+bash cmd_workers.sh start 2 1
+
+# Run a test server in the background
+python manage.py runserver 127.0.0.1:8000
+
