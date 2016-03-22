b480db4df6b42add59886aae42c893191634a7ce
camille24 <camille.marini@gmail.com>
2016-03-22 11:39:11 +0100

    script to run before deploying the master

---
+++ script_install/scp_master_stratuslab.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# :Usage: bash scp_master_stratuslab.sh {add_master}
+# Scp to a stratuslab instance (Ubuntu 14.04) files needed to deploy the master 
+# with {add_master} address pf the master
+
+ADD_MASTER=$1
+scp deploy_master_stratuslab.sh root@"$ADD_MASTER":/root/.
+scp env.sh root@"$ADD_MASTER":/root/.
