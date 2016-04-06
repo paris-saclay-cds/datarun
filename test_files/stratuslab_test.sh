bb276beb3897269ea1242edba8453b11eba332c8
camille24 <camille.marini@gmail.com>
2016-04-06 16:31:23 +0200

    no need to copy files to runner thanks to sciencefs

--- test_files/stratuslab_test.sh
+++ test_files/stratuslab_test.sh
@@ -6,19 +6,4 @@ MASTER=$1
 DR_USERNAME=$2
 DR_USERPSSD=$3
 
-################################################################
-# To be removed when we have a shared file system
-WORKER1=$4
-WORKER2=$5
-for WORKER in $WORKER1 $WORKER2 
-do
-    ssh root@$WORKER << EOF
-      mkdir /home/celery/data
-      mkdir /home/celery/submission
-EOF
-    scp -r ../test_data/* root@$WORKER:/home/celery/data/.
-    scp -r ../test_submission/* root@$WORKER:/home/celery/submission/.
-done
-################################################################
-
 python test_workflow.py http://"$MASTER" $DR_USERNAME $DR_USERPSSD
