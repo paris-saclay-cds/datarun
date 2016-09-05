f1b0998872d384e1f52365dcbe5b3b0995eec819
camille24 <camille.marini@gmail.com>
2016-09-05 14:19:19 +0200

    first try without tests

--- runapp/renderers.py
+++ runapp/renderers.py
@@ -2,4 +2,4 @@ from rest_framework.renderers import JSONRenderer
 
 
 class RunappJSONRenderer(JSONRenderer):
-    media_type = 'application/vnd.datarun.runapp+json
+    media_type = 'application/vnd.datarun.runapp+json'
